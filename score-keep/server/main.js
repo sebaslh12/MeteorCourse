15b628b820306925096deac9d90c589ca66b25ce
sebaslh12 <sebaslh12@gmail.com>
2019-04-21 18:11:39 -0500

    Creating a Mongo Collection. api folder created. Integrating mongo into the server.

--- score-keep/server/main.js
+++ score-keep/server/main.js
@@ -1,2 +1,10 @@
-console.log('Server is up!');
+import { Meteor } from 'meteor/meteor'
+import { Players } from '../imports/api/players'
 
+Meteor.startup(() => {
+    Players.insert({
+        name: 'Corey',
+        score: 99
+    })
+    console.log(Players.find().fetch())
+})
