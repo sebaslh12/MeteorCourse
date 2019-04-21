f56706ac6e668eae006d1b41212f2b21189eeeec
sebaslh12 <sebaslh12@gmail.com>
2019-04-21 17:19:04 -0500

    Installing and Exploring React and JSX

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -1,1 +1,10 @@
-console.log('Client is up!');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Meteor } from 'meteor/meteor'
+
+Meteor.startup(() => {
+    let name = 'Sebastian'
+    let jsx = <p>Hello {name}!</p>
+    let jsxContainer = document.querySelector('#app')
+    ReactDOM.render(jsx, jsxContainer)
+})
