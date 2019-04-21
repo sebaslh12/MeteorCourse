175447687e156f5c3990f4c2780a3d502def284b
sebaslh12 <sebaslh12@gmail.com>
2019-04-21 17:38:03 -0500

    Render complex HTML with JSX

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom'
 import { Meteor } from 'meteor/meteor'
 
 Meteor.startup(() => {
+    let title = 'Score Keep'
     let name = 'Sebastian'
-    let jsx = <p>Hello {name}!</p>
+    let jsx = (
+        <div>
+            <h1>{title}</h1>
+            <p>Hello {name}!</p>
+            <p>This is my second p</p>
+        </div>
+    )
     let jsxContainer = document.querySelector('#app')
     ReactDOM.render(jsx, jsxContainer)
 })
