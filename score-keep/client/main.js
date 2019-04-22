f29f7d87436fc6a69910a72ee4aa4aa37c17a8bc
sebaslh12 <sebaslh12@gmail.com>
2019-04-21 20:01:47 -0500

    Form to create players added

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -6,12 +6,18 @@ import { Tracker } from 'meteor/tracker'
 import { Players } from '../imports/api/players'
 
 const renderPlayers = (players) => players.map(({ _id: id, name, score }) => <p key={id}>{name} has {score} point(s)</p>)
-
+const handleSubmit = (e) => {
+    const playerName = e.target.playerName.value
+    if (playerName.trim()) {
+        Players.insert({
+            name: playerName,
+            score: 0
+        })
+        e.target.playerName.value = ''
+    }
+    e.preventDefault()
+}
 Meteor.startup(() => {
-    Players.insert({
-        name: 'Jen',
-        score: 15
-    })
 
     Tracker.autorun(() => {
         let players = Players.find().fetch()
@@ -23,6 +29,10 @@ Meteor.startup(() => {
                 <p>Hello {name}!</p>
                 <p>This is my second p</p>
                 {renderPlayers(players)}
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="playerName" placeholder="Player Name" />
+                    <button>Add Player</button>
+                </form>
             </div>
         )
         let jsxContainer = document.querySelector('#app')
