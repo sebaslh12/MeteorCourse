1528ce1b43cb250da1a69efdee8974368a685cee
sebaslh12 <sebaslh12@gmail.com>
2019-04-21 19:45:08 -0500

    Rendering Player Collection

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -1,37 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Meteor } from 'meteor/meteor'
+import { Tracker } from 'meteor/tracker'
 
-const players = [{
-    _id: '1',
-    name: 'Andrew',
-    score: 99
-},
-{
-    _id: '2',
-    name: 'Sebastian',
-    score: -1
-},
-{
-    _id: '3',
-    name: 'Corey',
-    score: -12
-}]
+import { Players } from '../imports/api/players'
 
 const renderPlayers = (players) => players.map(({ _id: id, name, score }) => <p key={id}>{name} has {score} point(s)</p>)
 
-
 Meteor.startup(() => {
-    let title = 'Score Keep'
-    let name = 'Sebastian'
-    let jsx = (
-        <div>
-            <h1>{title}</h1>
-            <p>Hello {name}!</p>
-            <p>This is my second p</p>
-            {renderPlayers(players)}
-        </div>
-    )
-    let jsxContainer = document.querySelector('#app')
-    ReactDOM.render(jsx, jsxContainer)
+    Players.insert({
+        name: 'Jen',
+        score: 15
+    })
+
+    Tracker.autorun(() => {
+        let players = Players.find().fetch()
+        let title = 'Score Keep'
+        let name = 'Sebastian'
+        let jsx = (
+            <div>
+                <h1>{title}</h1>
+                <p>Hello {name}!</p>
+                <p>This is my second p</p>
+                {renderPlayers(players)}
+            </div>
+        )
+        let jsxContainer = document.querySelector('#app')
+        ReactDOM.render(jsx, jsxContainer)
+    })
 })
--- score-keep/server/main.js
+++ score-keep/server/main.js
@@ -2,9 +2,5 @@ import { Meteor } from 'meteor/meteor'
 import { Players } from '../imports/api/players'
 
 Meteor.startup(() => {
-    Players.insert({
-        name: 'Corey',
-        score: 99
-    })
-    console.log(Players.find().fetch())
+    
 })
