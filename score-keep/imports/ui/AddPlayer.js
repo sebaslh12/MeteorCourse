b670ba01afe72fd67ea5e4c57973567ed0af4fa6
sebaslh12 <sebaslh12@gmail.com>
2019-05-11 16:39:19 -0500

    Styling Player component

--- score-keep/imports/ui/AddPlayer.js
+++ score-keep/imports/ui/AddPlayer.js
@@ -19,8 +19,8 @@ export default class AddPlayer extends React.Component {
     render() {
         return (
             <div className="item">
-                <form onSubmit={this.handleSubmit.bind(this)}>
-                    <input type="text" name="playerName" placeholder="Player Name" />
+                <form className="form" onSubmit={this.handleSubmit.bind(this)}>
+                    <input type="text" className="form__input" name="playerName" placeholder="Player Name" />
                     <button className="button">Add Player</button>
                 </form>
             </div>
--- score-keep/imports/ui/Player.js
+++ score-keep/imports/ui/Player.js
@@ -8,26 +8,31 @@ export default class Player extends React.Component {
         const { _id: id, name, score } = this.props.player
         return (
             <div className="item">
-                <p>
-                    {name} has {score} point(s)
-                </p>
-                <button className="button button--round" onClick={() => {
-                    Players.update(id,
-                        {
-                            $inc: {
-                                score: -1
-                            }
-                        })
-                }}>-1</button>
-                <button className="button button--round" onClick={() => {
-                    Players.update(id,
-                        {
-                            $inc: {
-                                score: 1
-                            }
-                        })
-                }}>+1</button>
-                <button className="button button--round" onClick={() => Players.remove(id)}>X</button>
+                <div className="player">
+                    <div>
+                        <h3 className="player__name">{name}</h3>
+                        <p className="player__stats">{score} point(s)</p>
+                    </div>
+                    <div className="player__actions">
+                        <button className="button button--round" onClick={() => {
+                            Players.update(id,
+                                {
+                                    $inc: {
+                                        score: -1
+                                    }
+                                })
+                        }}>-1</button>
+                        <button className="button button--round" onClick={() => {
+                            Players.update(id,
+                                {
+                                    $inc: {
+                                        score: 1
+                                    }
+                                })
+                        }}>+1</button>
+                        <button className="button button--round" onClick={() => Players.remove(id)}>X</button>
+                    </div>
+                </div>
             </div>
         )
     }
