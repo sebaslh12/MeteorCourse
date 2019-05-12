46f38b74f90a00a7b64243b806cfc523a5689ffe
sebaslh12 <sebaslh12@gmail.com>
2019-05-12 18:20:46 -0500

    Component state and Signup form

--- short-lnk/imports/ui/Signup.jsx
+++ short-lnk/imports/ui/Signup.jsx
@@ -2,14 +2,34 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class Signup extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            error: ''
+        }
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        this.setState({
+            error: 'Something went wrong'
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>Signup to Short Link</h1>
-                Signup form
+                {this.state.error ? <p>{this.state.error}</p> : undefined}
+                <form onSubmit={this.onSubmit}>
+                    <input type="email" name="email" placeholder="E-mail" />
+                    <input type="password" name="password" placeholder="password" />
+                    <button>Create account</button>
+                </form>
                 <Link to="/">Already have an account</Link>
             </div>
-            )
+        )
     }
 }
 
