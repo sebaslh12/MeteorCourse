b7fbda2d35cb22c7f5a7ef5f330541cc70a39685
sebaslh12 <sebaslh12@gmail.com>
2019-06-30 18:24:10 -0500

    Copying Link URL to clipboard

--- short-lnk/imports/ui/LinksListItem.jsx
+++ short-lnk/imports/ui/LinksListItem.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import ClipboardJS from 'clipboard'
 
 export default class LinksListItem extends React.Component {
+    componentDidMount() {
+        this.clipboard = new ClipboardJS(this.refs.copy)
+
+        this.clipboard.on('success', (e) => {
+            alert('Copied!')
+        })
+
+        this.clipboard.on('error', (e) => {
+            alert('Unable to copy.')
+        })
+    }
+
+    componentWillUnmount() {
+        this.clipboard.destroy();
+    }
+
     render() {
         return (
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
+                <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
             </div>
         )
     }
