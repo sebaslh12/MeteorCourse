07910370ac732576170ea3f938bdaef0f9f1617d
sebaslh12 <sebaslh12@gmail.com>
2019-08-12 11:21:14 -0500

    Controlled and Uncontrolled inputs.

--- short-lnk/imports/ui/AddLink.jsx
+++ short-lnk/imports/ui/AddLink.jsx
@@ -3,29 +3,44 @@ import { Meteor } from 'meteor/meteor'
 
 export default class AddLink extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.onSubmit = this.onSubmit.bind(this)
-    }
+	constructor(props) {
+		super(props);
+		this.onSubmit = this.onSubmit.bind(this);
+		this.onChange = this.onChange.bind(this);
+		this.state = {
+			url: ''
+		};
+	}
 
-    onSubmit(e) {
-        e.preventDefault()
-        const url = this.refs.url.value.trim();
-        if (url) {
-            Meteor.call('links.insert', url)
-            this.refs.url.value = ''
-        }
-    }
+	onSubmit(e) {
+		e.preventDefault()
+		const { url } = this.state;
+		if (url) {
+			Meteor.call('links.insert', url, (error, res) => {
+				if (!error) {
+					this.setState({
+						url: ''
+					});
+				}
+			});
+		}
+	}
 
-    render() {
-        return (
-            <div>
-                <p>Add link</p>
-                <form onSubmit={(this.onSubmit)}>
-                    <input type="text" ref="url" placeholder="URL" />
-                    <button>Add Link</button>
-                </form>
-            </div>
-        )
-    }
+	onChange(e) {
+		this.setState({
+			url: e.target.value.trim()
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<p>Add link</p>
+				<form onSubmit={(this.onSubmit)}>
+					<input type="text" placeholder="URL" value={this.state.url} onChange={this.onChange} />
+					<button>Add Link</button>
+				</form>
+			</div>
+		)
+	}
 }
