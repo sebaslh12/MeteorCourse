0434f7b2091227132f4258c0874cdd1d73e0e228
sebaslh12 <sebaslh12@gmail.com>
2020-01-31 16:54:33 -0500

    Testing note update

--- notes/imports/api/notes.test.js
+++ notes/imports/api/notes.test.js
@@ -5,16 +5,17 @@ import { Notes } from './notes';
 
 if (Meteor.isServer) {
 	describe('notes', function () {
+		const noteOne = {
+			_id: 'testNoteId1',
+			title: 'My title',
+			body: 'My body',
+			updatedAt: 0,
+			userId: 'testUserId1'
+		};
 
 		beforeEach(function () {
 			Notes.remove({});
-			Notes.insert({
-				_id: 'testNoteId1',
-				title: 'My title',
-				body: 'My body',
-				updatedAt: 0,
-				userId: 'testUserId1'
-			});
+			Notes.insert(noteOne);
 		})
 
 		it('should insert new note', function () {
@@ -30,20 +31,39 @@ if (Meteor.isServer) {
 		});
 
 		it('should remove note', function () {
-			Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' }, ['testNoteId1']);
-			expect(Notes.findOne({ _id: 'testNoteId1' })).toBeFalsy();
+			Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId }, [noteOne._id]);
+			expect(Notes.findOne({ _id: noteOne._id })).toBeFalsy();
 		});
 
 		it('should not remove note if unauthenticated', function () {
 			expect(() => {
-				Meteor.server.method_handlers['notes.remove'].apply({}, ['testNoteId1']);
+				Meteor.server.method_handlers['notes.remove'].apply({}, [noteOne._id]);
 			}).toThrow();
 		});
 
 		it('should not remove note if invalid _id', function () {
 			expect(() => {
-				Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' }, []);
+				Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId }, []);
 			}).toThrow();
 		});
+
+		it('should update note', function () {
+			const title = 'this is a new title';
+			Meteor.server.method_handlers['notes.update'].apply({ userId: noteOne.userId }, [noteOne._id, { title }]);
+			const updatedNote = Notes.findOne({ _id: noteOne._id });
+			expect(updatedNote.updatedAt).toBeGreaterThan(noteOne.updatedAt);
+			expect(updatedNote).toMatchObject({
+				title,
+				body: noteOne.body
+			});
+		});
+
+		it('should throw error if extra updates provided', function () {
+			expect(() => {
+				const title = 'this is a new title';
+				Meteor.server.method_handlers['notes.update'].apply({ userId: noteOne.userId }, [noteOne._id, { title, name: 'Sebas' }]);
+				const updatedNote = Notes.findOne({ _id: noteOne._id });
+			}).toThrow();
+		})
 	});
 }
